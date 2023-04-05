bfdb2f8438348d3d4a419e395af6780ad1f77717
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-04-05 14:53:19 +0300

    try6

--- frontend_try1/src/constants.ts
+++ frontend_try1/src/constants.ts
@@ -1,3 +1,5 @@
-const DEV_BACKEND_API_URL = "ec2-13-53-84-177.eu-north-1.compute.amazonaws.com/api"
+const PROD_BACKEND_API_URL = "/api";
+const DEV_BACKEND_API_URL = "ec2-13-53-84-177.eu-north-1.compute.amazonaws.com/api";
 
-export const BACKEND_API_URL = DEV_BACKEND_API_URL;
+export const BACKEND_API_URL =
+	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
