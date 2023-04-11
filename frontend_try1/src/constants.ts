8145c57e20d8ca9e85cd9920ae20d4c37e107d96
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-04-11 16:27:27 +0300

    try20

--- frontend_try1/src/constants.ts
+++ frontend_try1/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "https://ec2-13-50-101-56.eu-north-1.compute.amazonaws.com/api"
+const DEV_BACKEND_API_URL = "http://ec2-13-50-101-56.eu-north-1.compute.amazonaws.com/api"
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
