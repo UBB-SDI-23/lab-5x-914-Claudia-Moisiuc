064401dd7b4567d70f6f5ef7857e09de456558e7
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-04-13 14:46:13 +0300

    red44

--- frontend_try1/src/constants.ts
+++ frontend_try1/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "https://ec2-13-50-101-56.eu-north-1.compute.amazonaws.com/api"
+const DEV_BACKEND_API_URL = "https://ec2-13-51-45-89.eu-north-1.compute.amazonaws.com/api"
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
