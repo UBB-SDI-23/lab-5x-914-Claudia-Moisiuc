158d4897505445720209129e557bff7d5ed844d5
914-Claudia-Moisiuc <91720149+914-Claudia-Moisiuc@users.noreply.github.com>
2023-04-25 21:04:14 +0300

    front_try4

--- frontend_try1/src/constants.ts
+++ frontend_try1/src/constants.ts
@@ -1,4 +1,4 @@
-const DEV_BACKEND_API_URL = "https://mpp-try3-gallery.strangled.net/api/author/api"
+const DEV_BACKEND_API_URL = "https://mpp-try3-gallery.strangled.net/api/"
 //const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api"
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
